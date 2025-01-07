6112c1461bd63e2111ceb77a8506fe2e0d910ea2
Rodolfo Cupertino <rodolfols@gmail.com>
2025-01-07 09:11:02 -0300

    Update index.live.php
    Change theme to jupiter

--- index.live.php
+++ index.live.php
@@ -34,8 +34,8 @@ $user = exec('whoami');
 <?php
 
 //Store the output of the executed command in an array
-
-exec('/usr/local/cpanel/base/frontend/paper_lantern/fix/./fixperms.sh -a $USER', $output, $status);
+// exec('/usr/local/cpanel/base/frontend/paper_lantern/fix/./fixperms.sh -a $USER', $output, $status);
+exec('/usr/local/cpanel/base/frontend/jupiter/fix/./fixperms.sh -a $USER', $output, $status);
 
 
 //Print all return values of the executed command as array
